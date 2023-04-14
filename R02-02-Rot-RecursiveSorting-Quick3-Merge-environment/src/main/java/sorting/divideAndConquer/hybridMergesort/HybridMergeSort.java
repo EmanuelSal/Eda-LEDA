061e89fa64f4aa6587bb05aefb7115ed00a83a31
Evlima <limaemanuel06@gmail.com>
2023-04-14 11:26:34 -0300

    corrigir em casa onde tem marcação

--- R02-02-Rot-RecursiveSorting-Quick3-Merge-environment/src/main/java/sorting/divideAndConquer/hybridMergesort/HybridMergeSort.java
+++ R02-02-Rot-RecursiveSorting-Quick3-Merge-environment/src/main/java/sorting/divideAndConquer/hybridMergesort/HybridMergeSort.java
@@ -80,7 +80,7 @@ public class HybridMergeSort<T extends Comparable<T>> extends
             while (j >= leftIndex && array[j].compareTo(key) > 0) {
                 array[j + 1] = array[j];
                 j--;
-            }
+            } 
 
             array[j + 1] = key;
         }

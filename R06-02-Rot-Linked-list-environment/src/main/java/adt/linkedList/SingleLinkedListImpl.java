e6ed7468be493ca5b14dda7936ad144dd282b56b
EmanuelSal <limaemanuel06@gmail.com>
2023-05-12 11:47:43 -0300

    Fazer os testes

--- R06-02-Rot-Linked-list-environment/src/main/java/adt/linkedList/SingleLinkedListImpl.java
+++ R06-02-Rot-Linked-list-environment/src/main/java/adt/linkedList/SingleLinkedListImpl.java
@@ -1,9 +1,10 @@
 package adt.linkedList;
 
+
+
 public class SingleLinkedListImpl<T> implements LinkedList<T> {
 
 	protected SingleLinkedListNode<T> head;
-	private int elements;
 	
 	public SingleLinkedListImpl() {
 		this.head = new SingleLinkedListNode<T>();
@@ -19,8 +20,8 @@ public class SingleLinkedListImpl<T> implements LinkedList<T> {
 	public int size() {
 		int size = 0;
 		if(this.head.isNIL()) size = 0;  
-		SingleLinkedListNode aux = new SingleLinkedListNode<T>();
-		while (aux.isNIL()) {
+		SingleLinkedListNode<T> aux = this.head;
+		while (!aux.isNIL()) {
 			size ++;
 			aux.getNext();
 		} return size;
@@ -28,14 +29,36 @@ public class SingleLinkedListImpl<T> implements LinkedList<T> {
 
 	@Override
 	public T search(T element) {
+		T resulT = null;
+		SingleLinkedListNode<T> auxNode = this.head;
+		while(!auxNode.isNIL()) {
+			if (auxNode.getData().equals(element)) { 
+				resulT = auxNode.getData();
+				}
+			auxNode.getNext();
+		}
+		return resulT;
 		
 	}
 
 	@Override
 	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		SingleLinkedListNode<T> auxNode = this.head;
+		if (this.head.isNIL()) {
+			SingleLinkedListNode<T> newNode = new SingleLinkedListNode<>(element, new SingleLinkedListNode<T>());
+			this.head = newNode;
+		}
+		while(!auxNode.isNIL()) {
+			auxNode = auxNode.next;
+		}
+		auxNode.setData(element);
+		auxNode.setNext(new SingleLinkedListNode<>(element, new SingleLinkedListNode<>()));
+		head = auxNode;
+		
+		
+		
 	}
+	
 
 	@Override
 	public void remove(T element) {
@@ -44,9 +67,17 @@ public class SingleLinkedListImpl<T> implements LinkedList<T> {
 	}
 
 	@Override
+	@SuppressWarnings("unchecked")
 	public T[] toArray() {
+		T[] arraySaida = (T[]) new Object[this.size()];
+		SingleLinkedListNode<T> auxNode = this.head;
+		for (int i = 0; i < this.size(); i++) {
+			arraySaida[i] = auxNode.getData();
+			auxNode = auxNode.getNext();
+		}
+		return arraySaida;
 		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented!");
+		//throw new UnsupportedOperationException("Not implemented!");
 	}
 
 	public SingleLinkedListNode<T> getHead() {

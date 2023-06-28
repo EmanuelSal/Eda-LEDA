c788647f4af096a893ad55ad73086eeceae0287e
Evlima <limaemanuel06@gmail.com>
2023-06-28 09:41:51 -0300

    ver

---
+++ PR1-02-Prova-BinarySearchRotatedArray-environment/src/main/java/problems/testes.java
@@ -0,0 +1,37 @@
+package problems;
+
+public class testes {
+    public static void main(String[] args) {
+        int[] array = new int[] {9, 5, 6, 7, 8};
+        int retultado = findRotations(array);
+        System.out.println(retultado);
+    }
+    
+    public static int findRotations(int[] array){
+        if (array != null)
+            return binarySearchRotation(array, 0, array.length-1);
+        
+        return 0;
+    }   
+
+
+//TODO implement your code here
+// throw new UnsupportedOperationException("Not implemented yet!");
+
+
+public static int binarySearchRotation(int[] array, int left, int right) {
+    int rotation = 0;
+    int indiceControle = 0;
+    if (left < right) {
+        int meio = (left + right) /2;
+        if (array[left] > array[left +1]) {
+            indiceControle = left;
+            binarySearchRotation(array, indiceControle + 1, right);
+            binarySearchRotation(array, 0, indiceControle);
+            rotation +=1;
+        }
+    }
+    return rotation;
+} 
+}
+
